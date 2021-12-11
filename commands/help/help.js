0a0b5702047a5fa1a9663bb9ab764f3cb4ae395e
NarutoGaming <88095348+NarutoCodm@users.noreply.github.com>
2021-12-11 14:05:23 +0800

    Add files via upload

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,134 +1,118 @@
 const Discord = require('discord.js');
-const disbut = require("discord-buttons");
-const { MessageMenuOption, MessageMenu } = require("discord-buttons");
-const colors = require('./../../colors.json')
-
+const disbut = require('discord-buttons');
+const { MessageActionRow, MessageButton } = require("discord-buttons");
 
 module.exports = {
-  name: "help",
+  name: "tthelp",
    aliases: ["hlp"],
   run: async (client, message, args ) => {
       //--------------------------------------S T A R T---------------------------------------
 
-        const embed = new Discord.MessageEmbed()
-        .setTitle('')
-        .setFooter("Page Home\n"+ client.user.username + " | Made by: NarutoCodm", client.user.displayAvatarURL())
-        .addField("ZeroBot is a discord bot to help your server grow`",)
-        .setColor(colors.uptime);
+    //--------------------EMBEDS------------------------
 
-        const embed1 = new Discord.MessageEmbed()
-        .setTitle('<:jerryummmtf:912288891204157471> > Admin, Moderation/automod & Economy <:jerryummmtf:912288891204157471> ')
-        .addField("ㅤ⠀⠀⠀ \n <:jerryummmtf:912288891204157471>  **Admin** | <:jerryummmtf:912288891204157471>  **ENABLED**",
-          "`nuke` `react` `snipe` `dm`\n\n<:jerryummmtf:912288891204157471>  **Moderation** | <a:yes:865963544380964864> **ENABLED** \n `addrole` `ban` `purge` `hackban` `kick` `lock` `mute` `removerole` `slowmode` `timedlockdown` `unlock` `unmute` `addrole` `tempban` `warn` `warnings` `resetwarns`\n\n<:jerryummmtf:912288891204157471> **AutoModeration** | <:jerryummmtf:912288891204157471>  **ENABLED** \n `anti-alt` `autorole` `role-all`"
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`<a:arrow:904908180901994556> Click the buttons below to click the help menu!`);
+
+    const embed1 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle('<:8684_greentick:901827160799576145> Admin, Moderation/automod & Economy <:8684_greentick:901827160799576145>')
+        .addField("ㅤ⠀⠀⠀ \n <:8684_greentick:901827160799576145> **Admin** | <:8684_greentick:901827160799576145> **ENABLED**",
+          "`nuke` `react` `snipe` `dm`\n\n<:8684_greentick:901827160799576145> **Moderation** | <a:yes:865963544380964864> **ENABLED** \n `addrole` `ban` `purge` `hackban` `kick` `lock` `mute` `removerole` `slowmode` `timedlockdown` `unlock` `unmute` `addrole` `tempban` `warn` `warnings` `resetwarns`\n\n<:8684_greentick:901827160799576145>**AutoModeration** | <:8684_greentick:901827160799576145> **ENABLED** \n `anti-alt` `autorole` `role-all`"
+        )
+
+    const embed2 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+     .setTitle('<a:moderation7:905383548398211075> Info & help <a:moderation7:905383548398211075>')
+      .addField("⠀⠀⠀ \n <:8684_greentick:901827160799576145> **Info** | <:8684_greentick:901827160799576145> **ENABLED**",
+        "`badges` `botinfo` `roleinfo` `servericon` `serverinfo` `userinfo` `invite` `uptime` `avatar` \n\n<:8684_greentick:901827160799576145>> **Help** | <:8684_greentick:901827160799576145> **ENABLED**\n `bug` `help` `invite`\n\n<:8684_greentick:901827160799576145>**ECONOMY** | <:8684_greentick:901827160799576145> **ENABLED** \n  `balance` `buy` `daily` `deposit` `give` `inventory` `leaderboard` `rob` `shop` `use` `weekly` `withdraw` `work`"
+        )
+
+    const embed3 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle('<:8684_greentick:901827160799576145> Fun & Images <:8684_greentick:901827160799576145>')
+      .addField("ㅤ⠀⠀⠀ \n <:fun:866155255321853982> **Fun** | <:8684_greentick:901827160799576145> **ENABLED**","`ascii` `coinflip` `drake` `emojify` `joke` `meme` `rps` `slap` `trumptweet` `idp` `snipe` `github`\n\n <:8684_greentick:901827160799576145> **Image** | <:8684_greentick:901827160799576145> **ENABLED** \n`achievement` `captcha` `hug` `gay` `meeting` `rip` `tweet` `300yr` `anime` `comment` `slap` `wated`",)
+
+    const embed4 = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle(`<:8684_greentick:901827160799576145> Giveaway &Owner <:8684_greentick:901827160799576145>`)
+      .addField("ㅤ⠀⠀⠀ \n <:8684_greentick:901827160799576145>  **Giveaway** | <:8684_greentick:901827160799576145>  **Enable**",
+        "`$delete`, `$drop`, `$edit`, `$end`, `$help`, `$info`, `$reroll`, `$start`\n\n<:8684_greentick:901827160799576145> **Owner** | <:8684_greentick:901827160799576145> **ENABLED** \n`eval` `leaveserver` `serverlist` `status`\n\n<:8684_greentick:901827160799576145>**UTILITY** | <:8684_greentick:901827160799576145> **ENABLED** \n`vote` `advice` `findemoji` `members or membercount` `announce`\n\n<:8684_greentick:901827160799576145>**GAMES** | <:8684_greentick:901827160799576145> **ENABLED** \n`8ball`" //bro here 
         )
       .setThumbnail(client.user.displayAvatarURL())
-      .setColor(colors.uptime);
 
-        const embed2 = new Discord.MessageEmbed()
-     .setTitle('<:jerryummmtf:912288891204157471> > Info & help <:jerryummmtf:912288891204157471> ')
-      .addField("⠀⠀⠀ \n <:jerryummmtf:912288891204157471>  **Info** | <:jerryummmtf:912288891204157471>  **ENABLED**",
-        "`badges` `botinfo` `roleinfo` `servericon` `serverinfo` `userinfo` `invite` `uptime` `avatar` \n\n<:jerryummmtf:912288891204157471>  **Help** | <:jerryummmtf:912288891204157471>  **ENABLED**\n `bug` `help` `invite`\n\n<:jerryummmtf:912288891204157471> **ECONOMY** | <:jerryummmtf:912288891204157471>  **ENABLED** \n  `balance` `buy` `daily` `deposit` `give` `inventory` `leaderboard` `rob` `shop` `use` `weekly` `withdraw` `work`"
-        )
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(colors.uptime);
+    //--------------------EMBEDS------------------------
 
-        const embed3 = new Discord.MessageEmbed()
-      .setTitle('<:jerryummmtf:912288891204157471>  Fun & Images <:jerryummmtf:912288891204157471> ')
-      .addField("ㅤ⠀⠀⠀ \n <:fun:866155255321853982> **Fun** | <:jerryummmtf:912288891204157471>  **ENABLED**","`ascii` `coinflip` `drake` `emojify` `joke` `meme` `rps` `slap` `trumptweet` `idp` `snipe` `github`\n\n <:jerryummmtf:912288891204157471>  **Image** | <:jerryummmtf:912288891204157471>  **ENABLED** \n`achievement` `captcha` `hug` `gay` `meeting` `rip` `tweet` `300yr` `anime` `comment` `slap` `wated`\n\n <:jerryummmtf:912288891204157471>  **Leveling** | <:jerryummmtf:912288891204157471>  **ENABLED** \n`rank` `leaderboard`",)
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(colors.uptime);
+    //--------------------Buttons------------------------
 
-       const embed4 = new Discord.MessageEmbed()
-      .setTitle(`<:jerryummmtf:912288891204157471>  Giveaway &Owner <:jerryummmtf:912288891204157471> `)
-      .addField("ㅤ⠀⠀⠀ \n <:jerryummmtf:912288891204157471>   **Giveaway** | <:jerryummmtf:912288891204157471>   **Enable**",
-        "`gstart` `gend` `geroll` `ginfo` `gdrop` `gedit` `gdelete`\n\n<:jerryummmtf:912288891204157471>  **Owner** | <:jerryummmtf:912288891204157471>  **ENABLED** \n`eval` `leaveserver` `serverlist` `status`\n\n<:jerryummmtf:912288891204157471> **UTILITY** | <:jerryummmtf:912288891204157471>  **ENABLED** \n`vote` `advice` `findemoji` `members or membercount` `announce`\n\n<:jerryummmtf:912288891204157471> **GAMES** | <:jerryummmtf:912288891204157471>  **ENABLED** \n`8ball`")
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(colors.uptime);
+    let button1 = new MessageButton()
+    .setLabel(`Admin , Auto/Moderation`)
+    .setID(`help1`)
+    .setEmoji(`912288891204157471`)
+    .setStyle("green");
 
+    let button2 = new MessageButton()
+    .setLabel(`Info, Help & Economy`)
+    .setID(`help2`)
+    .setEmoji(`912288891204157471`)
+    .setStyle("green");
 
-       const embed5 = new Discord.MessageEmbed()
-      .setTitle(`<:jerryummmtf:912288891204157471>  Ticket <:jerryummmtf:912288891204157471> `)
-      .addField("ㅤ⠀⠀⠀ \n <:jerryummmtf:912288891204157471>  **Ticket** | <:jerryummmtf:912288891204157471> **ENABLED**",
-        "`$ticket (autosetup)`")
-      .setThumbnail(client.user.displayAvatarURL())
-      .setColor(colors.uptime);
+    let button3 = new MessageButton()
+    .setLabel(`Fun, Images & Leveling`)
+    .setID(`help3`)
+    .setEmoji(`912288891204157471`)
+    .setStyle("green");
 
-        //-----------------------------OPTIONS----------------------
+    let button4 = new MessageButton()
+    .setLabel(`Giveaway , Owner, UTILITY, Games`)
+    .setID(`help4`)
+    .setEmoji(`912288891204157471`)
+    .setStyle("green");
 
-        let option1 = new MessageMenuOption()
-        .setLabel('Admin , Auto/Moderation')
-        .setEmoji('916310968173207592')
-        .setValue('option1')
-        .setDescription('Admin , Auto/Moderation')
+    let row = new MessageActionRow()
+    .addComponents(button1, button2, button3, button4);
 
-        let option2 = new MessageMenuOption()
-        .setLabel('Info, Help & Economy')
-        .setEmoji('916310968173207592')
-        .setValue('option2')
-        .setDescription('Info , General & Aura')
+    //--------------------Buttons------------------------
 
-        let option3 = new MessageMenuOption()
-        .setLabel('Fun , Image & leveling')
-        .setEmoji('916310968173207592')
-        .setValue('option3')
-        .setDescription('Fun , Image & Nsfw')
+    const MESSAGE = await message.channel.send(embed, row);
 
-        let option4 = new MessageMenuOption()
-        .setLabel('giveaway, utitlty,games')
-        .setEmoji('916310968173207592')
-        .setValue('option4')
-        .setDescription('Vote our discord bot')
+    const filter = ( button ) => button.clicker.user.id === message.author.id 
+    const collector = MESSAGE.createButtonCollector(filter, { time : 120000 });
 
-        let option5 = new MessageMenuOption()
-        .setLabel('Tickets')
-        .setEmoji('916310968173207592')
-        .setValue('option5')
-        .setDescription('Ticket')
-        
-    let select = new MessageMenu()
-        .setID('selector')
-        .setPlaceholder('Click here to view the help menu!')
-        .setMaxValues(1)
-        .setMinValues(1)
-        .addOptions(option1, option2, option3, option4, option5)
+    collector.on('collect', async (b) => {
 
-        //-----------------------------OPTIONS----------------------
-    
-    const Sendmenu = await message.channel.send(embed, select);
+        if(b.id == "help1") {
 
-    const filter = ( button ) => button.clicker.user.id === message.author.id; //if only the message author click thenit will work
-    let collector = Sendmenu.createMenuCollector(filter, { time : 100000 });
+            MESSAGE.edit(embed1, row);
+            await b.reply.defer()
 
-    collector.on("collect" , (b) => {
-        if(b.values[0] == "option1") {
-            Sendmenu.edit(embed1, select)
         }
 
-        if(b.values[0] == "option2") {
-            Sendmenu.edit(embed2, select)
+        if(b.id == "help2") {
+            
+            MESSAGE.edit(embed2, row);
+            await b.reply.defer()
+
         }
 
-        if(b.values[0] == "option3") {
-            Sendmenu.edit(embed3, select)
+        if(b.id == "help3") {
+            
+            MESSAGE.edit(embed3, row);
+            await b.reply.defer()
+
         }
 
-        if(b.values[0] == "option4") {
-            Sendmenu.edit(embed4, select)
+        if(b.id == "help4") {
+            
+            MESSAGE.edit(embed4, row);
+            await b.reply.defer()
+
         }
 
-        if(b.values[0] == "option5") {
-            Sendmenu.edit(embed5, select)
-        }
 
-        if(b.values[0] == "option6") {
-            Sendmenu.edit(embed6, select)
-        }
-
-        b.reply.defer();
     })
 
-    collector.on("end", (b) => {
-        Sendmenu.edit("<a:no:865963806483808256> This help menu is expired! Please retype the command to view again.")
+    collector.on('end', (b) => {
+        MESSAGE.edit(`This help menu is expired! Type the command again to view.`)
     })
 
     }
